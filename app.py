14cd947930226b4d0bde58a5e8170038488d2f0d
ramizn <50529051+ramizn@users.noreply.github.com>
2019-05-11 21:25:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,62 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import database
+import os
 
 app=Flask(__name__)
-
 @app.route("/")
 def home():
-    return "Ramiz"
+    return render_template("home.html")
 
-app.run()
+
+
+@app.route("/newstd", methods=["POST","GET"])
+def newStd():
+    if request.method=="GET":
+        return render_template("/newstd.html")
+    data=request.form
+    fname=data["fname"]
+    lname=data["lname"]
+    no=data["no"]
+    bdate=data["bdate"]
+    dno=data["dno"]
+    database.addStd(no, fname, lname, bdate, dno)    
+    return "later"
+
+@app.route("/newcourse", methods=["POST","GET"])
+def newCourse():
+    if request.method=="GET":
+        return render_template("newcourse.html")
+    data=request.form
+    #cno int PRIMARY KEY, cname text, chours int, dno int, pid int
+    cname=data["cname"]
+    cno=data["cno"]
+    chours=data["chours"]
+    dno=data["dno"]
+    pid=data["pno"]
+    database.addCourse(cno, cname, chours, dno, pid)
+    return "later"
+
+@app.route("/newdep", methods=["POST","GET"])
+def newDepartment():
+    if request.method=="GET":
+        return render_template("newdep.html")
+    data=request.form
+    name=data["name"]
+    no=data["no"]
+    database.addDep(no,name)
+    return "later"
+
+@app.route("/newpre",methods=["POST","GET"])
+def addPre():
+    if request.method=="GET":
+        return render_template("newpre.html")
+    data=request.form
+    name=data["name"]
+    no=data["no"]
+    database.addPre(no,name)
+    return "later"
+
+
+os.system("rm uni.sql")
+database.init()
+app.run(debug=True)
